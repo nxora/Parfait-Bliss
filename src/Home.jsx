d666c1dc647eeb3fef1d39a3b305a443b00ac878
Davera <omotoyedave4@gmail.com>
2025-11-24 10:40:55 +0100

    Set up Tailwind

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './assets/Logo.png'
+import './index.css'
+
+function Home() {
+  return (
+    <div><img src={Logo} alt="" /></div>
+    
+  )
+}
+
+export default Home
