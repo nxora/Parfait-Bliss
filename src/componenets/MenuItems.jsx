3e729a98edd4e8d836de26c4a325931e0883cc0d
Davera <omotoyedave4@gmail.com>
2025-11-25 19:11:01 +0100

    Finished cards UI

--- src/componenets/MenuItems.jsx
+++ src/componenets/MenuItems.jsx
@@ -15,17 +15,17 @@ function MenuItems() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-6">
       {items.map(item => (
-        <div key={item._id} className="bg-white shadow-lg rounded-xl overflow-hidden">
+        <div key={item._id} className="text-center overflow-hidden">
           <img
             src={`./assets/${item.image_url}`} // use the correct field from DB
             alt={item.name}
-            className="w-full h-30em object-fill"          
+            className="w-1/2 h-30em object-fill rounded-lg m-auto"          
           />
           <div className="p-4">
             <h3 className="text-xl font-bold">{item.name}</h3>
-            <p className="text-gray-600 mt-1">{item.description}</p>
+            <p className="text-gray-600 mt-2 w-3/4 m-auto">{item.description}</p>
             <p className="mt-2 font-semibold">${item.price}</p>
             <p className={`mt-1 ${item.availability ? "text-green-600" : "text-red-600"}`}>
               {item.availability ? "Available" : "Out of stock"}
