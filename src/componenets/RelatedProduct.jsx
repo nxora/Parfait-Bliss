84f7871ff1fe7e85eb0608d7233ff9776de3a1b6
Davera <omotoyedave4@gmail.com>
2025-11-26 13:52:02 +0100

    Done

--- src/componenets/RelatedProduct.jsx
+++ src/componenets/RelatedProduct.jsx
@@ -32,16 +32,16 @@ function RelatedProduct() {
 
   return (
     <div>
-      <h1 className="font-semibold text-3xl ml-[20%]">Related Products</h1>
-      <hr className="w-[90%] m-auto bg-black border-black"/>
+      <h1 className="font-semibold text-3xl ml-[8%]">Related Products</h1>
+      <hr className="w-[85%] m-auto bg-black border-black"/>
 
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-6 m-14 ">
       {visibleItems.map(item => (
         <div key={item._id} className="text-center overflow-hidden rounded-lg transform transition-transform duration-300 hover:scale-105" onClick={() => navigate(`/menu/${item._id}`)}>
           <img
-            src={`./assets/${item.image_url}`} // use the correct field from DB
+            src={`./../assets/${item.image_url}`} // use the correct field from DB
             alt={item.name}
-            className="w-1/2 h-30em object-fill  m-auto hover:rounded-lg cursor-pointer tr"          
+            className="w-[80%] h-30em object-fill  m-auto hover:rounded-lg cursor-pointer tr"          
           />
           <div className="p-4">
             <h3 className="text-xl font-bold">{item.name}</h3>
